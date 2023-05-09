13358dfd39a49a66e905e27195ea286778758f6c
tonynhox <anhhuytran1470@gmail.com>
2023-05-10 00:50:16 +0700

    a

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -1,7 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit';
 import filterSlice from './reducer_slice/shop_slice/filterSlice';
 import itemSlice from './reducer_slice/shop_slice/itemSlice';
-import buyItemSlice from './reducer_slice/cart_slice/buyItemSlice';
 import loginSlice from './reducer_slice/user_slice/loginSlice';
 import getProductAPISlice from './reducer_slice/cart_slice/getProductAPISlice';
 import shopPageCategorySlice from './reducer_slice/shop_slice/shopPageCategorySlice';
@@ -10,7 +9,6 @@ export const store = configureStore({
   reducer: {
     filter: filterSlice,
     item: itemSlice,
-    buyItem: buyItemSlice,
     login: loginSlice,
     dataAPI: getProductAPISlice,
     dataCategoryMainShop: shopPageCategorySlice
